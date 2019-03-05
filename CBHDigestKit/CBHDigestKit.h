889767ee3ef33415676fca08d5dcea8f4de3f742
Chris Huxtable <chris@huxtable.ca>
2019-03-05 07:04:18 -0500

    Simplified Imports

--- CBHDigestKit/CBHDigestKit.h
+++ CBHDigestKit/CBHDigestKit.h
@@ -18,7 +18,8 @@
 //	OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 //
 
-@import Cocoa;
+@import Foundation.NSData;
+@import Foundation.NSString;
 
 FOUNDATION_EXPORT double CBHDigestKitVersionNumber;
 FOUNDATION_EXPORT const unsigned char CBHDigestKitVersionString[];
